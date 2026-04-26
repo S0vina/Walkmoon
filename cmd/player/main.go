8e589b3f386b300fd5a6b65b347f8a388ec2ff28
S0vina <hillan1236@gmail.com>
2026-04-26 11:13:46 -0300

    Addtio of pause, mute, increase volume and decrease volume in speaker ctrl

--- cmd/player/main.go
+++ cmd/player/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"bufio"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,6 +12,7 @@ import (
 	"github.com/gopxl/beep/v2"
 	"github.com/gopxl/beep/v2/mp3"
 	"github.com/gopxl/beep/v2/speaker"
+	"github.com/gopxl/beep/effects"
 )
 
 func main() {
@@ -71,10 +73,98 @@ func playAndWait(filePath string) {
 	// Inicializa o speaker com o formato da primeira música
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
+	ctrl := &beep.Ctrl{Streamer: beep.Loop(-1, streamer), Paused: false} 
+	volume := &effects.Volume{
+		Streamer: ctrl,
+		Base: 2,
+		Volume: 0,
+		Silent: false,
+	}
+	
+	scanner := bufio.NewScanner(os.Stdin)
+
 	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+	speaker.Play(beep.Seq(volume, beep.Callback(func() {
 		done <- true
 	})))
 
-	<-done // Bloqueia até a música terminar
+
+	
+	for {
+		// Logica de contagem de tempo 
+		// select {
+		// case <-done:
+		// 	return
+		// case <-time.After(time.Second):
+		// 	speaker.Lock()
+		// 	fmt.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
+		// 	speaker.Unlock()
+		// }
+
+		// Opcoes atuais de acao com o streamer volume
+		fmt.Println("\nPress [p] to pause/resume")
+        fmt.Println("Press [i] to increase volume")
+        fmt.Println("Press [d] to decrease volume")
+		fmt.Println("Press [m] to mute volume")
+        fmt.Print("-> ")
+
+        // Aguarda a entrada do usuário
+        if !scanner.Scan() {
+            break
+        }
+
+        // scanner.Text() pega a string e strings.TrimSpace remove espaços e o \n
+        resp := strings.TrimSpace(scanner.Text())
+
+		// Switch case com as opcoes de manipulacao do speaker possiveis
+        switch resp {
+        case "p":
+            speaker.Lock()
+            ctrl.Paused = !ctrl.Paused
+            speaker.Unlock()
+			if ctrl.Paused {
+				fmt.Println("Pausado")
+			} else{
+				fmt.Println("Despausado")
+			}
+            
+
+        case "i":
+			if volume.Volume < 3{
+				volume.Volume += 0.5
+				fmt.Println("Volume atual: %f", volume.Volume)
+				continue
+			}
+			fmt.Println("Volume maximo!!!")
+			
+
+        case "d":
+			if volume.Volume > -5 {
+				volume.Volume += -0.5
+				fmt.Println("Volume atual: %f", volume.Volume)
+				continue
+			}
+			fmt.Println("Volume minimo!!!")
+	
+		
+		case "m":
+			volume.Silent = !volume.Silent
+			if volume.Silent {
+				fmt.Println("Mutado")
+				continue
+			}
+			fmt.Println("Desmutado")
+
+        case "":
+            // Caso o usuário aperte Enter sem digitar nada, ignoramos
+            continue
+			
+        default:
+            fmt.Printf("Comando '%s' não reconhecido\n", resp)
+        }
+	}
 }
+
+// func decTypeArchive(f *os.File) {
+
+// }
