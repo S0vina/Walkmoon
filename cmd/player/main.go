d035bebd7ca932852e04cea06c64b2d8e1972c4e
S0vina <hillan1236@gmail.com>
2026-05-18 15:38:01 -0300

    Descentralizate the controllers initiate. Now, there is a func that initializates them and it perdures for all player execution. Wich solve multiple scanners and channels creation.

--- cmd/player/main.go
+++ cmd/player/main.go
@@ -20,51 +20,15 @@ type Musica struct{
 	Id int
 	Path string
 }
-    
-func main() {
-	fmt.Println("WELCOME TO THE WALKMOON!")
-	// Error: wrong args
-	if len(os.Args) < 2 {
-		fmt.Println("Uso: walkmoon <caminho_da_pasta>")
-		return
-	}
 
-	// path of the folder with the musics
-	folderPath := os.Args[1]
-
-	// songs = [archives], err = any error
-	playlist, err := scanFolder(folderPath)	
-
-	// if err is not null, break the program
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// if folder with no songs, break the program
-	if len(playlist) == 0 {
-		fmt.Println("Nenhuma musica foi encontrada.")
-		return
-	}
-	// debug quant of songs in the folder
-	fmt.Println("You got %d musics", len(playlist))
-
-	// init the speaker with the form of the file (SampleRate form)
-	sr := beep.SampleRate(44100)
-	speaker.Init(sr, sr.N(time.Second/10))
-
-	// for loop that play n songs of the folder
-	count := 0
-	for count < len(playlist){
-		song := playlist[count]
-
-		fmt.Printf("Tocando: [%d] %s\n", song.Id, filepath.Base(song.Path))
-		aux := playAndWait(song.Path, sr)
-		count += aux
-	}
-
-	fmt.Println("Todas as musicas foram tocadas")
+// Centralizes all components that perdure in AudioPlayer Execution
+type audioPlayer struct{
+	SampleRate beep.SampleRate
+	Ctrl       *beep.Ctrl
+	Volume     *effects.Volume
+	InputChan 	chan string
+	Scanner 	*bufio.Scanner
 }
-
 
 // Function that scans the folder passed in the arg and return the files in it
 func scanFolder(root string) (playlist []Musica, err error) {
@@ -91,10 +55,40 @@ func scanFolder(root string) (playlist []Musica, err error) {
 
 	return playlist, err
 }
-// function that creates a streamer with the actual file of the for loop and after that, playes it in
-// a speaker object
-func playAndWait(filePath string, sr beep.SampleRate) int {
-	// f = a file
+
+// func that creates a new audioPlayer
+func NewAP () (ap *audioPlayer, err error){
+	sr := beep.SampleRate(44100)
+
+	ctrl := &beep.Ctrl{Paused: false}
+
+	volume := &effects.Volume{
+		Streamer: ctrl,
+		Base: 2,
+		Volume: 0,
+		Silent: false,
+	}
+
+	inputChan := make(chan string, 1)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	speaker.Init(sr, sr.N(time.Second/10))
+
+	ap = &audioPlayer{
+		SampleRate: sr,
+		Ctrl: 		ctrl,
+		Volume:     volume,
+		InputChan: 	inputChan,
+		Scanner: 	scanner,
+	}
+
+	err = nil
+
+	return
+}
+
+func playAndWait(filePath string, ap *audioPlayer) int {
+	// f = a path for a song
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Println("Erro ao abrir arquivo:", err)
@@ -112,42 +106,25 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 	}
 	defer streamer.Close()
 
-	if format.SampleRate != sr {
-		beep.Resample(3, format.SampleRate, sr, streamer)
+	if format.SampleRate != ap.SampleRate {
+		beep.Resample(3, format.SampleRate, ap.SampleRate, streamer)
 		print("Speaker resampled in %d hz", format.SampleRate.N)
 	} 
 
-	// pause controller
-	ctrl := &beep.Ctrl{Streamer: beep.Loop(1, streamer), Paused: false} 
+	speaker.Lock()
+	ap.SampleRate = format.SampleRate
+	ap.Ctrl.Streamer = streamer
+	speaker.Unlock()
 
-	// volume controller
-	volume := &effects.Volume{
-		Streamer: ctrl,
-		Base: 2,
-		Volume: 0,
-		Silent: false,
-	}
-
-	// allows the end of the function
+	// trigger for the end of the function if the speaker.Play ends
 	done := make(chan bool, 1) // arg "1" allows a channel with buffer
 
-	speaker.Play(beep.Seq(volume, beep.Callback(func() {
+	speaker.Play(beep.Seq(ap.Volume, beep.Callback(func() {
 		done <- true
 	})))
 
-	// init a variable with a text scanner for input in the terminal
-	scanner := bufio.NewScanner(os.Stdin)
-
-	n_song := true
+	next_song := true
 	break_for := false
-
-	inputChan := make(chan string, 1)
-
-	go func() {
-		for scanner.Scan() {
-            inputChan <- strings.TrimSpace(scanner.Text())
-        }
-	}()
 
 	for {
 
@@ -164,15 +141,15 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 				fmt.Println("Playing next song...")
 				return 1
 
-			case resp := <-inputChan: 
+			case resp := <-ap.InputChan: 
 				// Switch case for player manipulation 
         		switch resp {
 				// pause player
         		case "p":
         		    speaker.Lock()
-        		    ctrl.Paused = !ctrl.Paused
+        		    ap.Ctrl.Paused = !ap.Ctrl.Paused
         		    speaker.Unlock()
-					if ctrl.Paused {
+					if ap.Ctrl.Paused {
 						fmt.Println("Pausado")
 					} else{
 						fmt.Println("Despausado")
@@ -180,26 +157,26 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 				
 				// increase volume
         		case "i":
-					if volume.Volume < 3{
-						volume.Volume += 0.5
-						fmt.Println("Volume atual: %f", volume.Volume)
+					if ap.Volume.Volume < 3{
+						ap.Volume.Volume += 0.5
+						fmt.Println("Volume atual: %f", ap.Volume.Volume)
 						continue
 					}
 					fmt.Println("Volume maximo!!!")
 					
 				// decrease volume
         		case "k":
-					if volume.Volume > -5 {
-						volume.Volume += -0.5
-						fmt.Println("Volume atual: %f", volume.Volume)
+					if ap.Volume.Volume > -5 {
+						ap.Volume.Volume += -0.5
+						fmt.Println("Volume atual: %f", ap.Volume.Volume)
 						continue
 					}
 					fmt.Println("Volume minimo!!!")
 				
 				// mute player
 				case "m":
-					volume.Silent = !volume.Silent
-					if volume.Silent {
+					ap.Volume.Silent = !ap.Volume.Silent
+					if ap.Volume.Silent {
 						fmt.Println("Mutado")
 						continue
 					}
@@ -207,14 +184,14 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 				
 				// jump for the previous song
 				case "j":
-					n_song = false
+					next_song = false
 					break_for = true
 					speaker.Clear()
 					break
 				
 				// jump for the next song
 				case "l":
-					n_song = true
+					next_song = true
 					break_for = true
 					speaker.Clear()
 					break
@@ -236,7 +213,7 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 
 	aux := 1
 
-	if (!n_song){
+	if (!next_song){
 		aux = -1
 	}  
 
@@ -257,3 +234,57 @@ func playAndWait(filePath string, sr beep.SampleRate) int {
 		// 	fmt.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
 		// 	speaker.Unlock()
 		// }
+    
+func main() {
+	fmt.Println("WELCOME TO THE WALKMOON!")
+	fmt.Println("------------------------")
+
+	// Error: wrong args
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: walkmoon <caminho_da_pasta>")
+		return
+	}
+
+	// path of the folder with the musics
+	folderPath := os.Args[1]
+
+	ap, errNewAP := NewAP() 
+
+	if errNewAP != nil {
+		log.Fatal(errNewAP)
+	}
+
+	go func() {
+		for ap.Scanner.Scan() {
+			ap.InputChan <- strings.TrimSpace(ap.Scanner.Text())
+		}
+	}()
+
+	// songs = [archives], err = any error
+	playlist, errScan := scanFolder(folderPath)	
+
+	// if ocurre some error in scanFolder func
+	if errScan != nil {
+		log.Fatal(errScan)
+	}
+	
+	// if folder with no songs, break the program
+	if len(playlist) == 0 {
+		fmt.Println("Nenhuma musica foi encontrada.")
+		return
+	}
+
+	// for loop that play n songs of the folder
+	count := 0
+	for count < len(playlist){
+		song := playlist[count]
+
+		fmt.Printf("Tocando: [%d] %s\n", song.Id, filepath.Base(song.Path))
+		aux := playAndWait(song.Path, ap)
+		count += aux
+	}
+
+	fmt.Println("Todas as musicas foram tocadas")
+}
+
+
