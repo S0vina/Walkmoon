63d7be1a1d4b3b7adef1bccc1d78de5a9b3f7fa0
S0vina <hillan1236@gmail.com>
2026-08-05 16:28:05 -0300

    Corrections: Erase line on .gitignore that blocks main.go file

--- cmd/player/main.go
+++ cmd/player/main.go
@@ -1,24 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	"encoding/json"
-	"fmt"
->>>>>>> ab294f2 (Feature: now, walkmoon have a shortcut created by install.sh.)
 	"log"
-<<<<<<< HEAD
-=======
-	"os"
-<<<<<<< HEAD
-	"encoding/json"
->>>>>>> b14e0f9 (Feat: now player can be initiated from that last state (music, volume, paused) that its left)
-=======
->>>>>>> ab294f2 (Feature: now, walkmoon have a shortcut created by install.sh.)
-=======
-	"log"
->>>>>>> 6cfe84f (Refact: Now, playerstate is created and updated in .config directory.)
 	"path/filepath"
 
 	"github.com/S0vina/walkmoon/internal/audio"
@@ -28,33 +11,14 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 	var ap *audio.AudioPlayer
 	var state *audio.PlayerState
 	var err error
 	var musicDir string
 	var playlist []audio.Musica
-=======
 
-=======
->>>>>>> ab294f2 (Feature: now, walkmoon have a shortcut created by install.sh.)
-	var state *audio.PlayerState
-	var defaultDir string
->>>>>>> b14e0f9 (Feat: now player can be initiated from that last state (music, volume, paused) that its left)
-
-<<<<<<< HEAD
 	state, musicDir, err = config.LoadState[audio.PlayerState]()
-=======
-	configPath := "../../assets/memory/playerState.json"
-
-	jsonPlayerState, err := os.ReadFile(configPath)
-
-	// caso o json ainda nao tenha sido criado
->>>>>>> ab294f2 (Feature: now, walkmoon have a shortcut created by install.sh.)
 	if err != nil {
-<<<<<<< HEAD
 		log.Printf("directory for musics has been loaded")
 	}
 
@@ -74,63 +38,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-=======
-		//home, errHome := os.UserHomeDir()
-		//if errHome != nil {
-		//	defaultDir = "assets/music"
-		//	log.Printf("err home: %s", errHome)
-		//}else {
-		//	defaultDir = home
-		//	log.Println(home)
-		//}
-=======
-	var ap *audio.AudioPlayer
-	var state *audio.PlayerState
-	var err error
-	var musicDir string
-	var playlist []audio.Musica
-
-	state, musicDir, err = config.LoadState[audio.PlayerState]()
-	if err != nil {
-		log.Printf("directory for musics has been loaded")
-	}
->>>>>>> 6cfe84f (Refact: Now, playerstate is created and updated in .config directory.)
-
-	ap, err = audio.New(state)
-	if err != nil {
-		log.Printf("Some problem ocurred in ap constructor", err)
-	}
-
-	if state != nil {
-		musicDir = filepath.Dir(state.PSlastTrackPath)
-		playlist, err = ap.ScanFolder(musicDir)
-	} else {
-		playlist, err = ap.ScanFolder(musicDir)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-<<<<<<< HEAD
-	// songs = [archives], err = any error
-	playlist, errScan := ap.ScanFolder(defaultDir)
-
-	// if ocurre some error in scanFolder func
-	if errScan != nil {
-		log.Fatal(errScan)
-	}
-
-	// if folder with no songs, break the program
-	if len(playlist) == 0 {
-		fmt.Println("No songs founded.")
-		return
-	}
-
-	// for loop that play n songs of the folder
->>>>>>> b14e0f9 (Feat: now player can be initiated from that last state (music, volume, paused) that its left)
-=======
->>>>>>> 6cfe84f (Refact: Now, playerstate is created and updated in .config directory.)
 	go func() {
 		ap.Run(playlist)
 	}()
@@ -146,8 +53,4 @@ func main() {
 	if err != nil {
 		log.Printf("PlayerState could not be created", err)
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6cfe84f (Refact: Now, playerstate is created and updated in .config directory.)
